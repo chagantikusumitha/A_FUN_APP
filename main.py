bd71637fd754f5702bc3ee54560a7cbfec7f06fc
chagantikusumitha <92007497+chagantikusumitha@users.noreply.github.com>
2023-02-27 01:23:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("Innomatics Data App")
+st.snow()
+
+btn_click = st.button("Click Me!")
+
+if btn_click == True:
+    st.subheader("You clicked me :cry:")
+    st.balloons()
